52bccc873dc78d5632664d5336f7b49a61018066
smpldvd <david.n.luu@gmail.com>
2018-06-07 17:46:30 -0500

    Added Bootstrap to html, styled css, and still working on JS.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,27 +2,45 @@
 $(document).ready(function () {
 
     // Start with initial array of GIFs
-    let subject = [];
+    let subject = ["Pokemon", "Mario", "Animaniacs", "Looney Tunes", "Simpsons", "Rick and Morty", "Thundercats", "Transformers"];
 
     // Have a function to dump JSON content of each button into div
     function displayGIF() {
+
         let $q = $(this).attr("data-name");
         let $apiKey = "wBIzukv6ZqgmL9LunKnFtolO0DZxGKzm";
-        let queryURL = `https://api.giphy.com/v1/gifs/trending?q=${$q}&api_key=${$apiKey}`;
+        let queryURL = `https://api.giphy.com/v1/gifs/search?q=${$q}&limit=10&api_key=${$apiKey}`;
 
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
-            console.log(response)
-        })
+            console.log(response);
+
+            // Clear the div before loading a new search
+            $("#gif-viewer").empty();
+            
+            for (var a = 0; a < subject.length; a++) {    
+                
+                let gifStill = response.data[a].images.fixed_height_still.url;
+                let gifPlay = response.data[a].images.fixed_height.url;
+                let gifImg = $("<img>").attr("data-state");
+                $("#gif-viewer").append(gifImg);
+                $(gifImg).hover(function() {
+                    console.log("click");
+                    $(this).attr("src", gifPlay);
+                });
+
+                renderBtn();
+            };
+        });
     }
 
     // As in lesson, function to display gif data
     function renderBtn() {
 
         // Clears the field prior to adding new input (important to avoid repeated buttons)
-        $("#gifBtn").empty();
+        $("#gif-buttons").empty();
 
         // Loop through array
         for (var i = 0; i < subject.length; i++) {
@@ -36,21 +54,25 @@ $(document).ready(function () {
             // Inputs text of subject
             a.text(subject[i]);
             // Appends button to the button div
-            $("#gifBtn").append(a);
+            $("#gif-buttons").append(a);
         }
     }
 
     // Click event function to handle GIF addition to array
     $("#add-gif").on("click", function (event) {
+
         event.preventDefault();
-        var gif = $("gif-input").val().trim();
+        
+        var gif = $("#gif-input").val().trim();
+        
         subject.push(gif);
-        renderButtons();
+        
+        renderBtn();
     });
 
     // Add click event listener to all element with gif class
-    $(document).on("click", ".movie", displayGIF);
+    $(document).on("click", ".gif", displayGIF);
 
     // Calls funcation to display initial buttons
-    renderButtons();
+    renderBtn();
 })
